17614a57b61367d68ea8d07cf031ed70330020b6
Emlyn Ma <emlyn.ma@gmail.com>
2023-12-17 22:52:51 +0800

    refactor

--- README.md
+++
@@ -1,2 +0,0 @@
-# my-spring
-Learn Spring 6 and Spring Boot 3
--- my-spring-base/joke-app/src/main/java/emlyn/ma/my/spring/joke/service/JokeService.java
+++
@@ -1,9 +0,0 @@
-package emlyn.ma.my.spring.joke.service;
-
-import java.util.List;
-
-public interface JokeService {
-
-    List<String> getJokeList(int count);
-
-}
