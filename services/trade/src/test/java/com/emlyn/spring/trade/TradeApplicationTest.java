7cfb26663e1ccabc3fb3ceeb5cbde0f94c3a09ea
emlyn ma <emlyn.ma@gmail.com>
2025-12-14 01:55:24 +0800

    feat: config

--- services/trade/src/test/java/com/emlyn/spring/trade/TradeApplicationTest.java
+++ services/trade/src/test/java/com/emlyn/spring/trade/TradeApplicationTest.java
@@ -1,5 +1,6 @@
 package com.emlyn.spring.trade;
 
+import com.emlyn.spring.trade.domain.config.CommonConfig;
 import com.emlyn.spring.trade.handler.RechargeHandler;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,9 @@ public class TradeApplicationTest {
 
     @Autowired
     private RechargeHandler rechargeHandler;
+
+    @Autowired
+    private CommonConfig commonConfig;
 
     @Test
     public void contextLoads() {
